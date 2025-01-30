9cae057f14ba8cb1febab988baa6d8caf29c9afb
johnny <johnny20150701@gmail.com>
2025-01-30 21:08:55 +0800

    update createOrder

--- api/createOrder.js
+++ api/createOrder.js
@@ -1,6 +1,7 @@
 import crypto from "crypto";
+import { nanoid } from "nanoid";
 
-function encryptAES256CBC (plaintext, key, iv) {
+function encryptAES256CBC(plaintext, key, iv) {
   if (key.length !== 32) {
     throw new Error("Key must be 32 bytes for AES-256.");
   }
@@ -14,7 +15,11 @@ function encryptAES256CBC (plaintext, key, iv) {
   encrypted += cipher.final("hex");
 
   return encrypted;
-};
+}
+
+function generateOrderNumber() {
+  return `LGB_${nanoid(10)}`; // Generates a 10-character unique ID
+}
 
 function sha256Hash(message) {
   const hash = crypto.createHash("sha256");
@@ -22,27 +27,27 @@ function sha256Hash(message) {
   return hash.digest("hex");
 }
 
-export function GET(request) {
+export function GET() {
   const params = {
     MerchantID: "MS148427498",
     RespondType: "String",
     TimeStamp: "1837289826",
     Version: "2.2",
-    MerchantOrderNo: "Line_Google_Drive_Bot_123",
+    MerchantOrderNo: generateOrderNumber(),
     Amt: 499,
     ItemDesc: "Line Google Drive機器人專業版",
-    NotifyURL: "https://webhook.site/d4db5ad1-2278-466a-9d66-78585c0dbadb",
   };
+  console.log(params);
   const queryString = new URLSearchParams(params).toString();
-  const hashKey = "zqr70wkJiJyXrpjoL1XWAgVDlNNIKnBs"
-  const hashIv = "C43Ge3BYGxLl7qmP"
+  const hashKey = "zqr70wkJiJyXrpjoL1XWAgVDlNNIKnBs";
+  const hashIv = "C43Ge3BYGxLl7qmP";
   const edata = encryptAES256CBC(queryString, hashKey, hashIv);
-  const hashs = `HashKey=${hashKey}&${edata}&HashIV=${hashIv}`
-  const hash = sha256Hash(hashs).toUpperCase()
+  const hashs = `HashKey=${hashKey}&${edata}&HashIV=${hashIv}`;
+  const hash = sha256Hash(hashs).toUpperCase();
   return Response.json({
-    "Version": "2.2",
-    "MerchantID": "MS148427498",
-    "TradeInfo": edata,   
-    "TradeSha": hash
+    Version: "2.2",
+    MerchantID: "MS148427498",
+    TradeInfo: edata,
+    TradeSha: hash,
   });
 }
